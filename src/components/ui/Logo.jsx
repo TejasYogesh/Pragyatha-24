ec08ee0b54086d36d66f9c9e33572201440374c1
Tejas Yogesh <yogeshtejaslalitha@gmail.com>
2024-11-08 07:52:10 +0530

    Logo

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -6,25 +6,25 @@ const Logo = () => {
         <div className='flex justify-items-start rounded-xl my-4'>
             <div>
                 <a href="https://www.facebook.com/profile.php?id=100095066721188" target="_blank" rel="noopener noreferrer">
-                    <img src="src/assets/Images/facebook.png" alt="Facebook" className='h-10 mr-12' />
+                    <img src="facebook.png" alt="Facebook" className='h-10 mr-12' />
                 </a>
             </div>
 
             <div>
                 <a href="https://www.linkedin.com/company/meriisefoundation/" target="_blank" rel="noopener noreferrer">
-                    <img src="src/assets/Images/linkedin.png" alt="LinkedIn" className='h-10 mr-12' />
+                    <img src="linkedin.png" alt="LinkedIn" className='h-10 mr-12' />
                 </a>
             </div>
 
             <div>
                 <a href="https://whatsapp.com/channel/0029VaiYmez9sBI88HYcLr47" target="_blank" rel="noopener noreferrer">
-                    <img src="src/assets/Images/whatsapp.png" alt="WhatsApp" className='h-10 mr-12' />
+                    <img src="whatsapp.png" alt="WhatsApp" className='h-10 mr-12' />
                 </a>
             </div>
 
             <div>
                 <a href="https://www.instagram.com/me_riise?igsh=NmxiNW1lbjRnODQym" target="_blank" rel="noopener noreferrer">
-                    <img src="src/assets/Images/Instagram.png" alt="Instagram" className='h-10 mr-12' />
+                    <img src="Instagram.png" alt="Instagram" className='h-10 mr-12' />
                 </a>
             </div>
         </div>
