a45ae8635712d91dc2b615e36767bacfe12eb41b
Tejas Yogesh <yogeshtejaslalitha@gmail.com>
2024-11-08 10:30:14 +0530

    Changs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,9 @@ const Footer = () => {
     return (
         <div>
             {/* <hr /> */}
-            <div className='bg-black text-center text-white text-xl p-4'>
-
-                <p className='p-8'>Pragyatha 2024 | All rights reserved</p>
-
+            <div className='bg-black text-white text-xl p-2 flex items-center justify-center'>
+                <p className='py-8 text-center'>Pragyatha 2024 | All rights reserved</p>
+                <img src="Screenshot 2024-09-12 100839.png" alt="" className='h-8 bg-white mx-4' />
             </div>
         </div>
     )
