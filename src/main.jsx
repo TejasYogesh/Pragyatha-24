c4833248b1e52888ad0f78fa87ae4337db35f83f
Tejas Yogesh <yogeshtejaslalitha@gmail.com>
2024-11-22 21:42:52 +0530

    errorElement

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
-    errorElement: <ErrorPage />
+    // errorElement: <ErrorPage />
   },
   {
     path: '/contact',
